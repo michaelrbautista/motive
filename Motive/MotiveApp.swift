a0472d6959c2b303d49a49ef6ccbb54bcbff385f
michael-bautista <mrbautistadev@gmail.com>
2025-04-27 11:04:28 -0700

    Initial Commit

---
+++ Motive/MotiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotiveApp.swift
+//  Motive
+//
+//  Created by Michael Bautista on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct MotiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MotiveTests/MotiveTests.swift
@@ -0,0 +1,17 @@
+//
+//  MotiveTests.swift
+//  MotiveTests
+//
+//  Created by Michael Bautista on 4/27/25.
+//
+
+import Testing
+@testable import Motive
+
+struct MotiveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
